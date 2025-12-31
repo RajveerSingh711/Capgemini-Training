81380855207466e6eed6f8fa7dae6b4c9631ebb3
RajveerSingh711 <rajveer032020@gmail.com>
2025-12-31 10:12:12 +0530

    Add README to main branch

--- Dec24/Welcome_to_BridgeLabz.java
+++
@@ -1,7 +0,0 @@
-package Dec24;// Write a program that prints "Welcome to Bridgelabz!" to the screen.
-
-public class Welcome_to_BridgeLabz {
-    public static void main(String[] args){
-        System.out.print("Welcome to Bridgelabz!");
-    }
-}
